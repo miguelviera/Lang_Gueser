3840476ba5bccfb9497c859fe4bc72e8affd1ce9
Miguel <43702390+miguelviera@users.noreply.github.com>
2021-03-05 08:17:02 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+let franc = require('franc');
+let langs = require('langs');
+let input = process.argv[2];
+let langCode = franc(input);
+let language = langs.where("3", langCode);
+
+console.log(language.name);
