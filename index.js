d90ff83db0a13a13c69361350e69ecd450120d77
Miguel <43702390+miguelviera@users.noreply.github.com>
2021-03-11 10:48:37 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-
+let franc = require('franc')
+let langs = require('langs');
+let input = process.argv[2];
+let langCode = franc(input);
+let language = langs.where("3", langCode);
 
 //Clases 
   class Cylon {
