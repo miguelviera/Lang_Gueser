0609abcf597da4d32696199bf234e4c366b3637c
Miguel <43702390+miguelviera@users.noreply.github.com>
2021-03-13 12:01:37 -0600

    Update index.js

--- index.js
+++ index.js
@@ -4,15 +4,6 @@ let input = process.argv[2];
 let langCode = franc(input);
 let language = langs.where("3", langCode);
 
-//Clases 
-  class Cylon {
-    constructor(model, attack) {
-       this.model = model
-       this.attack = function() {
-         return "Destroy all humans!"}    
-     }
-    
-  }
 
 
   
