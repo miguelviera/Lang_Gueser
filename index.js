c4492534950de9e5a9a614e26dfa01cc3036947d
Miguel <43702390+miguelviera@users.noreply.github.com>
2021-03-15 11:13:15 -0500

    Update index.js

--- index.js
+++ index.js
@@ -6,4 +6,19 @@ let language = langs.where("3", langCode);
 
 
 
+function flatten() {
+    let arr = []
+    let newArr = []
+    for (let i = 0; i < arguments.length; i++) {
+        arr = arr.concat(arguments[i])
+    }
+    for (let i = 0; i < 10; i++) {
+        for (let j = 0; j < arr.length; j++) {
+            newArr = newArr.concat(arr[j])
+        }
+        arr = newArr;
+        newArr = []
+    }
+    return arr;
+}
   
